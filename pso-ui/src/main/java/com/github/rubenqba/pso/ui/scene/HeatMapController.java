1122fad34bb19d68f0ccc1c596c733b8020cd154
Rubén Bresler Camps <ruben1981@gmail.com>
2017-03-22 19:43:48 -0600

    modificando la estructura para agregar una interfaz de usuario

---
+++ pso-ui/src/main/java/com/github/rubenqba/pso/ui/scene/HeatMapController.java
@@ -0,0 +1,17 @@
+package com.github.rubenqba.pso.ui.scene;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by ruben.bresler on 22/03/17.
+ */
+public class HeatMapController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
